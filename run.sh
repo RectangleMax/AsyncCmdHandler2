8bcf3fc43175236b6c06f29af29cc50e8a8e694c
RectangleMax <mkubatkin@yandex.ru>
2024-10-16 23:18:43 +0300

    fix run.sh for single launch

--- run.sh
+++ run.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/bash
-if [ "$#" -ne 2 ]; then
-    echo "Need two input argument: 1 - num of threads, 2 - num of run"
-    exit
-fi
-if [ -f temp.txt ]; then
-    rm temp.txt;
-fi
 
-# clang++ easyMain.cpp asyncHandler.cpp TaskHandler.cpp TaskOutputer.cpp
-clang++ easyMain.cpp asyncHandler.cpp TaskHandler.cpp TaskOutputer.cpp -fsanitize=thread
-for ((i = 0; i < $2; ++i)); do { ./a.out $1; } &>>temp.txt; done
-grep -c -i "seg" temp.txt
-
+clang++ main.cpp asyncHandler.cpp TaskHandler.cpp TaskOutputer.cpp -fsanitize=thread -std=c++20
+./a.out 3 input1.txt
